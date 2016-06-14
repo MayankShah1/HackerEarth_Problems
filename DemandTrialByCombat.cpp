21422f4f395985f07fce6107aeaead83d0746f52
Mayank Shah <mayankshah11996@gmail.com>
2016-06-14 10:25:00 +0530

    Update DemandTrialByCombat.cpp

--- DemandTrialByCombat.cpp
+++ DemandTrialByCombat.cpp
@@ -19,43 +19,43 @@ int main()
 		    long int count_0 = 0;
 		    long int count_1 = 0;
 
-            int temp = position[0];
+            	    int temp = position[0];
 
-            if(temp)
-                count_1++;
-            else
-                count_0++;
+                    if(temp)
+                	count_1++;
+                    else
+                	count_0++;
 
 		    position[0] = position[1];
-			for(int i = 1; i < lannisters -1; i++)
-			{
-			    if(position[i])
-                    count_1++;
+		    for(int i = 1; i < lannisters -1; i++)
+		    {
+		      if(position[i])
+                    	count_1++;
+                      else
+                    	count_0++;
+
+		      if(temp ==1 && position[i+1] ==1)
+                      {
+                    	temp = position[i];
+			position[i] = 1;
+                      }
+                      else
+                      {
+                    	temp = position[i];
+			position[i] = 0;
+		      }
+		}
+		if(position[lannisters-1])
+                	count_1++;
                 else
-                    count_0++;
+                	count_0++;
 
-				if(temp ==1 && position[i+1] ==1)
-                {
-                    temp = position[i];
-					position[i] = 1;
-                }
-                else
-                {
-                    temp = position[i];
-					position[i] = 0;
-				}
-			}
-			if(position[lannisters-1])
-                count_1++;
-            else
-                count_0++;
+		position[lannisters - 1] = temp;
 
-			position[lannisters - 1] = temp;
-
-			if(count_0 == lannisters || count_1 == lannisters)
-            {
-                break;
-            }
+		if(count_0 == lannisters || count_1 == lannisters)
+            	{
+                	break;
+            	}
 
 		}
 		for(int i = 0; i < lannisters; i++)
